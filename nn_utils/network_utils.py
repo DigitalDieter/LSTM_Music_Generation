be5d77f70ab2aae41a20d8e20ba2cc9222895fa2
DigitalDieter <devops@backupmailer.de>
2019-07-15 22:32:37 +0200

    network_utils.py updated

--- nn_utils/network_utils.py
+++ nn_utils/network_utils.py
@@ -12,19 +12,20 @@ CONFIG = nn_config.get_neural_net_configuration()
 ACTIVATION = CONFIG['activation']
 STATEFUL = CONFIG['stateful']
 
+FREQ_SPACE_DIMS = CONFIG['num_frequency_dimensions']
 NUM_HIDDEN_DIMENSIONS = CONFIG['hidden_dimension_size']
 NUM_RECURRENT_UNITS = CONFIG['num_recurrent_units']
 
-def create_lstm_network(num_frequency_dimensions, NUM_HIDDEN_DIMENSIONS, NUM_RECURRENT_UNITS):
+def create_lstm_network(FREQ_SPACE_DIMS, NUM_HIDDEN_DIMENSIONS, NUM_RECURRENT_UNITS):
     model = Sequential()
     # Layer converts frequency space to hidden space
     model.add(TimeDistributed(Dense(NUM_HIDDEN_DIMENSIONS, activation=ACTIVATION),
-                              input_shape=(None, num_frequency_dimensions)))
+                              input_shape=(None, FREQ_SPACE_DIMS)))
 
     for cur_unit in range(NUM_RECURRENT_UNITS):
         model.add(LSTM(units=NUM_HIDDEN_DIMENSIONS, return_sequences=True, stateful=STATEFUL))
     # Layer converts hidden space back to frequency space
-    model.add(TimeDistributed(Dense(input_dim=NUM_HIDDEN_DIMENSIONS, output_dim=num_frequency_dimensions,
+    model.add(TimeDistributed(Dense(input_dim=NUM_HIDDEN_DIMENSIONS, output_dim=FREQ_SPACE_DIMS,
                                     activation=ACTIVATION)))
 
     # Done building the model.Now, configure it for the learning process
