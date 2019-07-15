13953ee15d544a6d6c91fc789c7c2745bfee06fc
DigitalDieter <devops@backupmailer.de>
2019-07-16 00:33:10 +0200

    train.py updated

--- train.py
+++ train.py
@@ -9,16 +9,21 @@ from matplotlib import pyplot as plt
 import numpy as np
 import nn_utils.network_utils as network_utils
 import config.nn_config as nn_config
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-
-#warnings.filterwarnings("ignore", category=FutureWarning)
-#warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.simplefilter("ignore")
-#DeprecationWarning
 
+CONFIG = nn_config.get_neural_net_configuration()
+CUR_ITER = 0
+FREQ_SPACE_DIMS = CONFIG['num_frequency_dimensions']
+HIDDEN_DIMS = CONFIG['hidden_dimension_size']
+INPUTFILE = CONFIG['model_file']
+MODEL_BASENAME = CONFIG['model_basename']
+MODEL_FILENAME = MODEL_BASENAME + str(CUR_ITER)
+NUM_RECURR = 1
 
 parser = argparse.ArgumentParser(description='Train model')
+
 parser.add_argument('-n', '--n_iter', type=int, default=5,
                     help='number of iterations')
 parser.add_argument('-e', '--n_epochs', type=int, default=3,
@@ -26,19 +31,12 @@ parser.add_argument('-e', '--n_epochs', type=int, default=3,
 parser.add_argument('-b', '--n_batch', type=int, default=5,
                     help='batchsize per iterations')
 
-args = parser.parse_args()
 
+args = parser.parse_args()
 
 NUM_ITERS = args.n_iter
 EPOCHS_PER_ITER = args.n_epochs
 BATCH_SIZE = args.n_batch
-
-
-CONFIG = nn_config.get_neural_net_configuration()
-INPUTFILE = CONFIG['model_file']
-CUR_ITER = 0
-MODEL_BASENAME = CONFIG['model_basename']
-MODEL_FILENAME = MODEL_BASENAME + str(CUR_ITER)
 
 # Load up the training data
 print('Loading training data')
@@ -50,11 +48,6 @@ X_TRAIN = np.load(INPUTFILE + '_x.npy')
 Y_TRAIN = np.load(INPUTFILE + '_y.npy')
 
 print('Finished loading training data')
-
-# Figure out how many frequencies we have in the data
-FREQ_SPACE_DIMS = CONFIG['num_frequency_dimensions']
-HIDDEN_DIMS = CONFIG['hidden_dimension_size']
-NUM_RECURR = 1
 
 # Creates a lstm network
 MODEL = network_utils.create_lstm_network(FREQ_SPACE_DIMS=FREQ_SPACE_DIMS,
@@ -77,7 +70,6 @@ while CUR_ITER < NUM_ITERS:
         for listitem in LOSS:
             filehandle.write('%s\n' % listitem)
     CUR_ITER += 1
-
 
 print('Training complete!')
 MODEL.save_weights(WEIGHTS_PATH +".h5")
